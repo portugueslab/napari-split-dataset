b5a99cf5fd6327fec49e2d594344aa9257fb41a9
You <ykure.w@gmail.com>
2021-02-18 17:16:31 +0100

    Bump version: 0.1.0 → 0.1.1

--- napari_split_dataset/__init__.py
+++ napari_split_dataset/__init__.py
@@ -1,6 +1,6 @@
 """hdf5/split-dataset file reader for napari"""
 
 __author__ = """You Wu @portugueslab"""
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from napari_split_dataset.napari_split_dataset import napari_get_reader
